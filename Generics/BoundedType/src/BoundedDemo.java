de810bca2cb2a946cab089c144ebe6eb6cf1fc0a
Chavan <rohan-gorakhnath.chavan@capgemini.com>
2025-02-09 21:09:57 +0530

    Added Simple WildCard

--- Generics/BoundedType/src/BoundedDemo.java
+++ Generics/BoundedType/src/BoundedDemo.java
@@ -1,10 +1,11 @@
 public class BoundedDemo {
     public static void main(String[] args) {
         Integer[] intArrays={1,2,3,4};
-        Double[] doubleArr={1.1,2.2,3.3,4.4};
+        Double[] doubleArr={1.0,2.0,3.0,4.0};
         BoundedType<Integer>intArray=new BoundedType<>(intArrays);
         BoundedType<Double>doubleArray=new BoundedType<>(doubleArr);
         intArray.getAverage();
         doubleArray.getAverage();
+        intArray.compareAverage(doubleArray);
     }
 }
--- Generics/BoundedType/src/BoundedType.java
+++ Generics/BoundedType/src/BoundedType.java
@@ -4,11 +4,18 @@ public class BoundedType <T extends Number>{//Bound Type to Number
     BoundedType(T[] o){
         numbers=o;
     }
-    public void getAverage(){
+    public double getAverage(){
         for(T num:numbers){
             sum+=num.doubleValue();
         }
         System.out.println("Sum : "+sum);
         System.out.println("Average : "+sum/numbers.length);
+        return sum/numbers.length;
+    }
+    public void compareAverage(BoundedType<?> ob){
+        if(ob.getAverage()==getAverage()){
+            System.out.println("Equal");
+        }
+        else System.out.println("not Equal");
     }
 }
