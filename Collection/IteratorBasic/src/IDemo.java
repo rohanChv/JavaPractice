35a3b083633448cb32ccb9174e54ef9acc59c144
Chavan <rohan-gorakhnath.chavan@capgemini.com>
2025-02-20 10:36:23 +0530

    collection practice 2

---
+++ Collection/IteratorBasic/src/IDemo.java
@@ -0,0 +1,27 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class IDemo {
+    public static void main(String[] args) {
+        List<Integer> intList=new LinkedList<>();
+        long timeBefore=System.currentTimeMillis();
+        for(int i=0;i<1000000;i++){
+            intList.add(i);
+        }
+        System.out.println("Enter number to be found in Linked List");
+        Scanner sc =new Scanner(System.in);
+        int a=sc.nextInt();
+        Iterator<Integer> listIterator=intList.iterator();
+        while (listIterator.hasNext()){
+            if(listIterator.next().equals(a)){
+                System.out.println("element present");
+                long operationEndTime=System.currentTimeMillis();
+                System.out.println("time taken "+(operationEndTime-timeBefore));
+                break;
+            }
+        }
+
+    }
+}
