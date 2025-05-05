ddb512884e3269ff910b288229f365828f0fa43d
Rohan Chavan <rohan-gorakhnath.chavan@capgemini.com>
2025-05-05 15:59:34 +0530

    inheritance Example

---
+++ Inheritance/BasicInheritance/src/Animal.java
@@ -0,0 +1,9 @@
+public class Animal {
+    String name="NA";
+    public void commonProp(){
+        System.out.println("I can Eat,breath");
+    }
+    Animal(){
+        this.name="Nepali";
+    }
+}
